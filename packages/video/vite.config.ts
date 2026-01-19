a0667374111aac5aff8aed63dfee0d336952202b
bigmistqke <vandijckv@gmail.com>
2026-01-19 12:59:14 +0100

    chore: enable sourcemaps for library packages

--- packages/video/vite.config.ts
+++ packages/video/vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true })],
+  plugins: [dts({ tsconfigPath: './tsconfig.json' })],
   build: {
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
